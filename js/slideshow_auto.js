c4f420de2004603b84f308cc9495dd772a06898d
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2019-06-01 12:42:53 +0800

    slick test

--- js/slideshow_auto.js
+++ js/slideshow_auto.js
@@ -1,19 +1,14 @@
-var slideIndex = 0;
-showSlides();
+$(document).ready(function(){
+  $('.slideshow_container').slick({
+    setting-name: setting-value
+  });
+});
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000);
-}
+$('.autoplay').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
+  pauseOnDotsHover: true;
+});
