95706d07120a8ba4c8a3a1831fdb4ab55e6b8a2b
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2019-06-02 22:35:29 +0800

    hid the works button (nothing there so far)

---
+++ js/modal_img.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('myModal');
+
+var img = $('.myImg');
+var modalImg = $("#img01");
+var captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
