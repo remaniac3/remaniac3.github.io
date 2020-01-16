65cbdd84dc6dc1301d3c613fd5961601d8a66492
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2020-01-16 18:15:10 +0800

    refined the "scroll to top" button

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,41 +1,27 @@
-mybutton = document.getElementById("backToTop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		mybutton.style.display = "block";
-	} else {
-		mybutton.style.display = "none";
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 200) {
+		$('#backToTop').fadeIn();
+	} else { $('#backToTop').fadeOut();
 	}
+});
+
+
+function checkOffset() {
+    if($('#backToTop').offset().top + $('#backToTop').height() 
+                                           >= $('#footerEx').offset().top - 10)
+        $('#backToTop').css({'position': 'fixed', 'margin-bottom': '6em'});
+    if($(document).scrollTop() + window.innerHeight < $('#footerEx').offset().top)
+        $('#backToTop').css({'position': 'fixed', 'margin-bottom': '0'});
 }
 
-function functionTop() {
-	document.body.scrollTop = 0; //Safari
-	document.documentElement.scrollTop = 0; //Everything else.
-}
+$(document).scroll(function() {
+    checkOffset();
+});
 
 
-  //   $(document).ready(function() {		
-  //     $(window).scroll(function() {
-  //  //--------------------------- Lines added ------------------------//
-  //      var footertotop = ($('.footer').position().top);
-  //      var scrolltop = $(document).scrollTop() + window.innerHeight;
-  //      var difference = scrolltop-footertotop;
-  //     if (scrolltop > footertotop) {
-  //       $('.go-top').css({'bottom' : difference});
-  //     }else{
-  //       $('.go-top').css({'bottom' : 10});
-  //     };   
-  // //--------------------------- end ---------------------------------//
-  //     if ($(this).scrollTop() > 200) {
-  //         $('.go-top').fadeIn(200);
-  //     } else {
-  //         $('.go-top').fadeOut(200);
-  //     }
-  //     });
-  //     $('.go-top').click(function(event) {
-  //       event.preventDefault();
-  //       $('html, body').animate({scrollTop: 0}, 300);
-  //     })
-  //   });
+$('#backToTop').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500);
+	return false;
+});
