9ae3fb05eca7f37c826935431ef95258d8d63c90
R3MANIAC <50291272+remaniac3@users.noreply.github.com>
2020-02-07 17:46:27 +0800

    some fixes
    - fixed the back to top button appearance
    - standardised footer (only applicable to socmed links)

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,24 +1,29 @@
+// Hide the button in the beginning.
+$('#backToTop').hide();
+
+// Where the magic happens.
 $(window).scroll(function() {
 	if ($(this).scrollTop() > 200) {
 		$('#backToTop').fadeIn();
-	} else { $('#backToTop').fadeOut();
-	}
+	} else { $('#backToTop').fadeOut()};
 });
 
-
+// Offset from footer so no overlapping.
 function checkOffset() {
-    if($('#backToTop').offset().top + $('#backToTop').height() 
-                                           >= $('#footerEx').offset().top - 10)
-        $('#backToTop').css({'position': 'fixed', 'margin-bottom': '6em'});
-    if($(document).scrollTop() + window.innerHeight < $('#footerEx').offset().top)
-        $('#backToTop').css({'position': 'fixed', 'margin-bottom': '0'});
-}
+	if($('#backToTop').offset().top + $('#backToTop').height() 
+	                                       >= $('#footerEx').offset().top - 10)
+    $('#backToTop').css({'position': 'fixed', 'margin-bottom': '6em'});
 
+    if($(document).scrollTop() + window.innerHeight < $('#footerEx').offset().top)
+    $('#backToTop').css({'position': 'fixed', 'margin-bottom': '0'});
+};
+
+// Self-explanatory.
 $(document).scroll(function() {
     checkOffset();
 });
 
-
+// The actual work.
 $('#backToTop').click(function() {
 	$('html, body').animate({
 		scrollTop: 0
